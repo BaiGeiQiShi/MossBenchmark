a48c2c6126dc0328f9f9e097f9cfa2adbe5ad554
Unprincess17 <2020302111121@whu.edu.cn>
2023-08-05 00:25:58 +0800

    fix: change size calculating methond. Remove .base

--- vim-5.8/getsize.sh
+++ vim-5.8/getsize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 PROGNAME=vim-5.8
-TIMEOUT=2
+TIMEOUT=4
 GDTBIN=ROPgadget
 DOMGAD=/usr/local/Moss/CovPath
 #PROG_COUNTER_BIN="/home/qxin6/progcounter/build/bin/counter"
@@ -58,10 +58,10 @@ rm -rf debdcetmp
 
 
 #Count Binary Bytes
-original_size=$((`ls -l ${ORIGIN_BIN} | cut -d' ' -f5`-`ls -l $BASE_BIN | cut -d' ' -f5`))
-reduced_size=$((`ls -l ${BIN} | cut -d' ' -f5`-`ls -l $BASE_BIN | cut -d' ' -f5`))
-#original_size=$(ls -l ${ORIGIN_BIN} | cut -d' ' -f5)
-#reduced_size=$(ls -l ${BIN} | cut -d' ' -f5)
+#original_size=$((`ls -l ${ORIGIN_BIN} | cut -d' ' -f5`-`ls -l $BASE_BIN | cut -d' ' -f5`))
+#reduced_size=$((`ls -l ${BIN} | cut -d' ' -f5`-`ls -l $BASE_BIN | cut -d' ' -f5`))
+original_size=$(ls -l ${ORIGIN_BIN} | cut -d' ' -f5)
+reduced_size=$(ls -l ${BIN} | cut -d' ' -f5)
 
 #Count Gadgets
 original_gdt=`${GDTBIN} --binary ${ORIGIN_BIN} | grep 'Unique gadgets' | cut -d' ' -f4`
